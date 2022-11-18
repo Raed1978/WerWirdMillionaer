30eaaec3bc77f9d412898e44d36b502f86111193
raed1978 <raedmohammad617@gmail.com>
2022-11-18 19:51:37 +0100

    5050 joker

--- taschenrechner.py
+++ taschenrechner.py
@@ -1,3 +1,4 @@
+joker = 1
 print("Willkommen zu 'Wer wird Millionär?'")
 print("Was ist die Hauptstadt von Deutschland?")
 print("a) München")
@@ -6,17 +7,28 @@ print("c) Berlin")
 print("d) Hamburg")
 antwort = input("Bitte geben Sie Ihre Antwort.\n")
 print("Ihre Antwort ist: " + antwort)
-if (antwort == "Berlin"):
+if (antwort == "5050" and joker == 1):
+    print("a) München")
+    print("c) Berlin")
+    joker = 0
+antwort = input("Bitte geben Sie Ihre Antwort.\n")
+if (antwort == "Berlin" or antwort == "berlin" or antwort == "c"):
     print("Ihre Antwort ist richtig!")
 else:
     print("Ihre Antwort ist leider falsch.")
+    quit("Du hast verloren")
 print("was ist der flüss zwischen deudscland umd polen ")
-print("oder")
-print("poo")
-print("rein")
-print("spree")
+print("a) oder")
+print("b) poo")
+print("c) Rhein")
+print("D) spree")
 antwort = input("geben sie bitte ihr antwort \n")
-if (antwort=="oder"):
+if (antwort == "5050" and joker == 1):
+    print("a) Oder")
+    print("c) Rhein")
+    joker = 0
+antwort = input("geben sie bitte ihr antwort \n")
+if (antwort=="oder" or antwort == "Oder" or antwort == "a"):
     print("ihre antwort ist richtig")
 else: 
     print("ihre antwort ist fasch ")   
