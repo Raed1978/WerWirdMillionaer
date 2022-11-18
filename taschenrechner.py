7d0330e0a6b607c6127a0ed8e17714a82f8b1841
raed1978 <raedmohammad617@gmail.com>
2022-11-18 19:18:59 +0100

    initial commit

---
+++ taschenrechner.py
@@ -0,0 +1,12 @@
+print("Willkommen zu 'Wer wird Millionär?'")
+print("Was ist die Hauptstadt von Deutschland?")
+print("a) München")
+print("b) Brandenburg a. d. Havel")
+print("c) Berlin")
+print("d) Hamburg")
+antwort = input("Bitte geben Sie Ihre Antwort.\n")
+print("Ihre Antwort ist: " + antwort)
+if (antwort == "Berlin"):
+    print("Ihre Antwort ist richtig!")
+else:
+    print("Ihre Antwort ist leider falsch.")
